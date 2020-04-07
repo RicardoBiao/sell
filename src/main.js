90debcdee03c08525d29db30b7047d1a44df5d17
Ricardo <532569829@qq.com>
2020-04-08 00:28:59 +0800

    添加vue-router

--- src/main.js
+++ src/main.js
@@ -2,11 +2,27 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
-import router from './router';
+// import router from './router';
 import VueRouter from 'vue-router';
+import goods from '../src/components/goods/goods';
+import seller from '../src/components/seller/seller';
 
 Vue.config.productionTip = false
 Vue.use(VueRouter);
+
+const routes = [
+    { path: '/goods', component: goods },
+    { path: '/seller', component: seller }
+  ]
+
+var router =  new VueRouter({
+    routes
+})
+
+const app = new Vue({
+    router
+  }).$mount('#app')
+export default router;
 
 /* eslint-disable no-new */
 new Vue({
@@ -15,3 +31,7 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
+
+
+
+
